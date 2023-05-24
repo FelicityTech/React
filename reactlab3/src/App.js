03b0c35b973a50d6a300e21fef77eb000ed24014
Solomon Eniola Adegoke <Souceking@gmail.com>
2023-05-24 12:20:44 +0100

    props

---
+++ reactlab3/src/App.js
@@ -0,0 +1,11 @@
+import Heading from "./Heading";
+
+function App() {
+    return (
+        <div className="App">
+            <Heading firstName="Bob" firstName="Sola"/>
+        </div>
+    );
+};
+
+export default App;
